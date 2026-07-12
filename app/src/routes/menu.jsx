9fa808b2961f89576936896e890e2961cb2f2685
nabeellagi <nabeellagi@gmail.com>
2026-07-13 06:39:53 +0700

    New art and new dynamic menu page

--- app/src/routes/menu.jsx
+++ app/src/routes/menu.jsx
@@ -1,20 +1,54 @@
 import { PixelButton } from '@/components/PixelButton'
 import Title from '@/components/title'
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
 
 export const Route = createFileRoute('/menu')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return(
-    <div className="flex flex-col items-center justify-center">
-      <Title>
-        MENU
-      </Title>
+  const bg = useTimeOfDayBg();
+  return (
+    <div
+      className="flex flex-col items-center
+      w-full h-screen [image-rendering:pixelated]"
+      style={{
+        backgroundImage: `url('${bg}')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}
+    >
       <div>
-        
-      </div>     
+        <Title>
+          MENU
+        </Title>
+      </div>
     </div>
   )
 }
+
+/**
+ * Background will change based on time. 
+ * There are exactly two variations! Day and sunset ig.
+*/
+const useTimeOfDayBg = () => {
+  const getBg = () => {
+    const hour = new Date().getHours();
+    return hour <= 15 ? '/bg/day.png' : '/bg/sunset.png';
+  }
+  const [bg, setBg] = useState(getBg);
+
+  useEffect(() => {
+    const interval = setInterval(() => setBg(getBg()), 60000);
+    return () => clearInterval(interval);
+  }, [])
+
+  return bg;
+}
+
+/**
+TODO :
+1. Bg change
+2. Back btn
+ */
